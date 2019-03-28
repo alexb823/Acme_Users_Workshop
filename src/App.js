5d599291cbcd01153d91820db48fa36f46abfed6
Alex Bukhman <alexb823@gmail.com>
2019-03-27 20:08:03 -0400

    refactored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import UsersTable from './UsersTable';
 
 export default class App extends Component {
